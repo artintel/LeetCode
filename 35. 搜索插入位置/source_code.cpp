e34a0011d836e063819b500f127efcad744973ff
Mayichao <3074099684@qq.com>
2020-11-24 11:06:13 +0800

    update source

--- 35. 搜索插入位置/source_code.cpp
+++ 35. 搜索插入位置/source_code.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n = nums.size();
-        int left = 0, right = n - 1, ans = n;
-        while (left <= right) {
-            int mid = ((right - left) >> 1) + left;
+
+        if (nums.size() == 0) return 0;
+        if (nums[nums.size() - 1] < target) return nums.size();
+
+        int left = 0, right = n - 1;
+        while (left != right) {
+            int mid = ((right + left) >> 1);
             if (target <= nums[mid]) {
-                ans = mid;
-                right = mid - 1;
-            } else {
+                right = mid;
+            }
+            else {
                 left = mid + 1;
             }
         }
-        return ans;
+        return left;
     }
 };
