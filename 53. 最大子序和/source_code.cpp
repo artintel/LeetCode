6a493117ae6e2d3e38a50714db688799e5de7070
Mayichao <3074099684@qq.com>
2020-11-25 09:38:38 +0800

    53. 最大子序和

---
+++ 53. 最大子序和/source_code.cpp
@@ -0,0 +1,46 @@
+# 动态规划
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int temp = 0, MAX = INT_MIN;
+        for(const auto& x : nums){
+            temp = max(temp + x, x);
+            MAX = max(temp, MAX);
+        }
+        return MAX;
+    }
+};
+
+# 分治法
+class Solution {
+public:
+    struct Status {
+        int lSum, rSum, mSum, iSum;
+    };
+
+    Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = max(l.lSum, l.iSum + r.lSum);
+        int rSum = max(r.rSum, r.iSum + l.rSum);
+        int mSum = max(max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return (Status) {lSum, rSum, mSum, iSum};
+    };
+
+    Status get(vector<int> &a, int l, int r) {
+        if (l == r) {
+            return (Status) {a[l], a[l], a[l], a[l]};
+        }
+        int m = (l + r) >> 1;
+        Status lSub = get(a, l, m);
+        Status rSub = get(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        return get(nums, 0, nums.size() - 1).mSum;
+    }
+};
+
+
+
+
