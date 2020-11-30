0c794c743f0ad02ffd2188c2e969bd4038355b81
Mayichao <3074099684@qq.com>
2020-11-30 15:00:20 +0800

    283. 移动零

---
+++ 283. 移动零/source_code.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        queue<int> q;
+            int len = nums.size();
+            for(int i=0;i<len;i++){
+                if(nums[i] != 0){
+                    q.push(nums[i]);
+                }
+            }
+            while(q.size()<len){
+                q.push(0);
+            }
+            for(int i=0;i<len;i++){
+                nums[i] = q.front();
+                q.pop();
+            }
+    }
+};
+
+作者：chen-532
+链接：https://leetcode-cn.com/problems/move-zeroes/solution/yong-dui-kuai-su-jie-jue-by-chen-532/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
