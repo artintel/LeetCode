11dc77a9704a136eacc26b818fab0a3fedd44568
Mayichao <3074099684@qq.com>
2020-12-06 13:35:40 +0800

    392.判断子序列 dp算法

--- 392. 判断子序列/source_code.cpp
+++ 392. 判断子序列/source_code.cpp
@@ -15,3 +15,21 @@ public:
         return S.size() == 0;
     }
 };
+
+
+# dp
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        vector<vector<int>> dp(s.length() + 1, vector<int>(t.length() + 1, 0));
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 1; j <= t.length(); j++){
+                if(s[i - 1] != t[j - 1]){
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
+                else dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+        }
+        return dp[s.length()][t.length()] == s.length();
+    }
+};
