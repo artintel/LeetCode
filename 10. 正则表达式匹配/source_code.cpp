b1f5d9772d4961fa44bdde2bc89c2eea8532f549
Mayichao <3074099684@qq.com>
2020-12-08 15:12:56 +0800

    10. 正则表达式匹配

---
+++ 10. 正则表达式匹配/source_code.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<bool>> dp(s.size() + 1, vector<bool>(p.size() + 1, false));
+        dp[0][0] = true;
+        for (int i = 0; i <= s.size(); i++) {
+            for (int j = 1; j <= p.size(); j++) {
+                if (p[j - 1] != '*') {
+                    if (i == 0) dp[i][j] = false;
+                    else if (s[i - 1] == p[j - 1] || p[j - 1] == '.') dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (i != 0 && p[j - 2] == s[i - 1]) {
+                        dp[i][j] = (dp[i - 1][j] | dp[i][j]);
+                    }
+                    else if (i != 0 && p[j - 2] == '.') {
+                        dp[i][j] = (dp[i - 1][j] | dp[i][j]);
+                    }
+                }
+            }
+        }
+        return dp[s.size()][p.size()];
+    }
+};
