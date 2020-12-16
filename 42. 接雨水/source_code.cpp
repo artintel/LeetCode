6e467292fd80140e49418a32c1297ae8bd4b5cd5
Mayichao <3074099684@qq.com>
2020-12-16 10:44:11 +0800

    update readme

--- 42. 接雨水/source_code.cpp
+++ 42. 接雨水/source_code.cpp
@@ -1,3 +1,4 @@
+# DP
 class Solution {
 public:
     int trap(vector<int>& height)
@@ -20,3 +21,27 @@ public:
         return ans;
     }
 };
+
+# double point
+int trap(vector<int>& height)
+{
+    int left = 0, right = height.size() - 1;
+    int ans = 0;
+    int left_max = 0, right_max = 0;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
+            ++left;
+        }
+        else {
+            height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
+            --right;
+        }
+    }
+    return ans;
+}
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/trapping-rain-water/solution/jie-yu-shui-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
