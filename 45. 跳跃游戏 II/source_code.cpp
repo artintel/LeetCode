4f953c997277bba3644737516c4fd8529059c484
Mayichao <3074099684@qq.com>
2020-12-18 10:40:30 +0800

    跳跃游戏II

---
+++ 45. 跳跃游戏 II/source_code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int maxPos = 0, n = nums.size(), end = 0, step = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (maxPos >= i) {
+                maxPos = max(maxPos, i + nums[i]);
+                if (i == end) {
+                    end = maxPos;
+                    ++step;
+                }
+            }
+        }
+        return step;
+    }
+};
