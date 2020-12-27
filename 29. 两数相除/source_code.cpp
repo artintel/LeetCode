3214c35306f07e70be83a189e9561dc55398715e
Mayichao <3074099684@qq.com>
2020-12-27 14:14:34 +0800

    29. 两数相除

---
+++ 29. 两数相除/source_code.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend == 0) return 0;
+        if(divisor == 1) return dividend;
+        if(divisor == -1){
+            if(dividend>INT_MIN) return -dividend;// 只要不是最小的那个整数，都是直接返回相反数就好啦
+            return INT_MAX;// 是最小的那个，那就返回最大的整数啦
+        }
+        long a = dividend;
+        long b = divisor;
+        int sign = 1; 
+        if((a>0&&b<0) || (a<0&&b>0)){
+            sign = -1;
+        }
+        a = a>0?a:-a;
+        b = b>0?b:-b;
+        long res = div(a,b);
+        if(sign>0)return res>INT_MAX?INT_MAX:res;
+        return -res;
+    }
+    int div(long a, long b){  // 似乎精髓和难点就在于下面这几句
+        if(a<b) return 0;
+        long count = 1;
+        long tb = b; // 在后面的代码中不更新b
+        while((tb+tb)<=a){
+            count = count + count; // 最小解翻倍
+            tb = tb+tb; // 当前测试的值也翻倍
+        }
+        return count + div(a-tb,b);
+    }
+};
+作者：liujin-4
+链接：https://leetcode-cn.com/problems/divide-two-integers/solution/po-su-de-xiang-fa-mei-you-wei-yun-suan-mei-you-yi-/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
