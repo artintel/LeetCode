b8f228a5793e02cc56c636ee23c13ef65836fdc0
Mayichao <3074099684@qq.com>
2021-01-08 13:21:44 +0800

    78. 子集

---
+++ 78. 子集/source_code.cpp
@@ -0,0 +1,54 @@
+/*
+ * @Author: Mayc
+ * @Date: 2021-01-08 11:16:20
+ * @LastEditTime: 2021-01-08 11:27:16
+ * @FilePath: \undefinedd:\VSC++&GO\LeetCode\78. 子集\source_code.cpp
+ * @这是一个漂亮的注释头
+ */
+// 字典序
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        for(int mask = 0; mask < (1 << n); mask++){
+            temp.clear();
+            for(int i = 0; i < n; i++){
+                if( mask & (1 << i) ) {
+                    temp.push_back(nums[i]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+// 递归
+class Solution {
+public:
+    vector<int> t;
+    vector<vector<int>> ans;
+
+    void dfs(int cur, vector<int>& nums) {
+        if (cur == nums.size()) {
+            ans.push_back(t);
+            return;
+        }
+        t.push_back(nums[cur]);
+        dfs(cur + 1, nums);
+        t.pop_back();
+        dfs(cur + 1, nums);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        dfs(0, nums);
+        return ans;
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/subsets/solution/zi-ji-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
